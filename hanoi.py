914c8979a1eaef135c8a194c19ededac39e43488
Sergio Zavarce <sergiozavarce@gmail.com>
2021-10-31 16:57:48 +0100

    hanoi update

--- hanoi.py
+++ hanoi.py
@@ -5,21 +5,26 @@ class Hanoi:
     memo = {}
     steps = 0
     def __init__(self, disks):
+        """ Constructor for the class """
         for disk in range(disks, 0, -1):
           self.pegA.append(disk)
 
     def displayHanoi(self):
+        """ Used if disks will be shown """
         self.arrange(disks, self.pegA, self.pegC, self.pegB)
         return self.steps
 
     def calculateHanoi(self):
+        """ method to calculate without showing the steps """
         result = self.calculateOnly(disks, self.memo)
         return result       
 
     def __str__(self):
+        """ method to display the results """ 
         return (f"peg A: {self.pegA}\npeg B: {self.pegB}\nPeg C: {self.pegC}\n--------")
 
     def arrange(self, n, moveFrom, aux, moveTo):
+        """ logic for disk arrangement """
         if n == 0:
             print(self)
             return            
@@ -30,6 +35,7 @@ class Hanoi:
         self.arrange(n - 1, aux, moveFrom, moveTo)
 
     def calculateOnly(self, n, dictionary):
+        """ method for calculation only using memoization """
         if n < 2:
             return n
         if n in dictionary:
@@ -40,19 +46,19 @@ class Hanoi:
 
 
 if __name__ == "__main__":
+    """ 
+    Main program. Depending of the number of disks it will show the steps or not.
+    If number of disks is greater than 6, it will use a memoization approach to
+    optimize the number of steps.
+    """
     disks = int(input("Number of disks: "))
     tower = Hanoi(disks)
     if disks < 7:
-      result = tower.displayHanoi()
-      print(f"Total steps: {result}")
+        result = tower.displayHanoi()
+        print(f"Total steps: {result}")
     elif disks > 998:
-      print("Maximum amount exceeded")
+        print("Maximum amount exceeded")
     else:
-      result = tower.calculateHanoi()
-      print("Skipping display. Using memoization:")
-      print(f"Total steps: {result}")
-
-
-
-
-
+        result = tower.calculateHanoi()
+        print("Skipping display. Using memoization:")
+        print(f"Total steps: {result}")
