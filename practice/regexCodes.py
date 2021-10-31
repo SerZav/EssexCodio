f5a07632c7226c5dfbe9b936a67b3d512072a101
Sergio Zavarce <sergiozavarce@gmail.com>
2021-10-31 17:02:18 +0100

    regex update

--- practice/regexCodes.py
+++ practice/regexCodes.py
@@ -1,13 +1,21 @@
 import re
 
-pattern = r"(?P<outcode>[A-Za-z][A-Za-z0-9]{1,2}[A-Za-z0-9]?)[ ](?P<incode>([0-9][A-Za-z^CIKMOV][A-Za-z^CIKMOV]))$"
-sequence = "AA11 AA"
+# regex code with inner description of parameters
+pattern = r"(?P<outcode>[A-Za-z][A-Za-z0-9]{1,2}[A-Za-z0-9]?)[ ](?P<incode>"
+patter += "([0-9][A-Za-z^CIKMOV][A-Za-z^CIKMOV]))$"
 print(re.match(pattern,sequence) != None)
 
-tests="M1 1AA","M60 1NW","CR2 6XH","DN55 1PT","W1A 1HQ","EC1A 1BB"
-testsF=" ", "  ", "   ", "        ","M1  1AA","M6 0 1NW","CR2 A6XH","DN55A 1PT","W 1A 1HQ","EC 1A 1B","CR2AA6XH","CR2A6XH"
-for test in tests:
+# some valid codes
+testsOk = "M1 1AA", "M60 1NW", "CR2 6XH", "DN55 1PT", "W1A 1HQ", "EC1A 1BB"
+
+# some invalid codes
+testsFail = " ", "  ", "   ", "        ", "M1  1AA", "M6 0 1NW", "CR2 A6XH",
+"DN55A 1PT", "W 1A 1HQ", "EC 1A 1B", "CR2AA6XH", "CR2A6XH"
+
+# test cases for accepted codes
+for test in testsOk:
     print(f"testing ok: '{test}' - Result: {re.match(pattern,test) != None}")
 
-for test in testsF:
+# test cases for failing codes
+for test in testsFail:
     print(f"testing fail: '{test}' - Result: {re.match(pattern,test) != None}")
