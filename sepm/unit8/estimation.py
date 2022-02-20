0faf4df8debb99cc706e086ad23380e690b922cd
Sergio Zavarce <sergiozavarce@gmail.com>
2022-02-20 19:09:00 +0100

    cocomo correciton

--- sepm/unit8/estimation.py
+++ sepm/unit8/estimation.py
@@ -1,33 +1,56 @@
-class cocomo:
-    a=0
-    b=0
-    c=0
-    d=0
-    def __init__(self, type):
-        if type == 1:
-            self.a,self.b,self.c,self.d = self.organic()
-        elif type == 2:
-            self.a,self.b,self.c,self.d = self.semiDetached()
-        elif type == 3:
-            self.a,self.b,self.c,self.d = self.embedded()
-        
-        
+""" Estimation test """
+class Cocomo:
+    """ Estimation for Cocomo method """
+    effort = 0.0
+    time = 0.0
+    staff = 0.0
+    selected_type = ""
+    size = 0
+    def __init__(self, kloc):
+        self.size = kloc
+        self.selected_type = self.get_type()
+        if self.selected_type == "organic":
+            a, b, c, d = self.organic()
+        elif self.selected_type == "semi detached":
+            a, b, c, d = self.semi_detached()
+        elif self.selected_type == "embedded":
+            a, b, c, d = self.embedded()
+        self.effort = a * kloc ** b
+        self.time =  c * self.effort **  d
+        self.staff = self.effort / self.time
 
-            
-    def cocomo(self):
-        """ """
-        
-    def organic(self):
+    @staticmethod
+    def organic():
+        """ Organic values """
         return (2.4, 1.05, 2.5, 0.38)
 
-    def semiDetached(self):
-        return (3.0, 1.12, 2.5, 0.35)        
+    @staticmethod
+    def semi_detached():
+        """ Semi Detached Values """
+        return (3.0, 1.12, 2.5, 0.35)
 
-    def embedded(self):
-        return (3.6, 1.20, 2.5, 0.32)        
+    @staticmethod
+    def embedded():
+        """ Embedded values"""
+        return (3.6, 1.20, 2.5, 0.32)
 
-    
+    def get_type(self):
+        if self.size < 50:
+            return "organic"
+        elif self.size <= 300:
+            return "semi detached"
+        else:
+            return "embedded"
 
-a = cocomo(1)
-print(a.a)
+    def __str__(self):
+        return "Lines of code: " + str(self.size * 1000) + \
+        "\nMode: " + self.selected_type + \
+        "\nEffort: " + str(self.effort) + \
+        "\nTime: " + str(self.time) + \
+        "\nStaff: " + str(self.staff)
 
+print(Cocomo(5))
+print("-----")
+print(Cocomo(150))
+print("-----")
+print(Cocomo(350))
